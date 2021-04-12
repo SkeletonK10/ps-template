66802e16f5cc82d15141579b15be22f53ad5674f
SkeletonK10 <66668299+SkeletonK10@users.noreply.github.com>
2021-04-12 18:23:20 +0900

    Update union-find.cpp

--- union-find.cpp
+++ union-find.cpp
@@ -1,24 +1,24 @@
 struct Union_Find
 {
-   std::vector<int> UF;
-   
-   Union_Find(int sz)
-   {
-       UF.resize(sz + 1);
-      for (int i = 1; i <= sz; i++)
-         UF[i] = i;
-   }
-
-   int Find(int x)
-   {
-       if (UF[x] == x) return x;
-       else return UF[x] = Find(UF[x]);
-   }
-
-   void Union(int x, int y)
-   {
-      x = Find(x);
-      y = Find(y);
-      UF[y] = x;
-   }
+    std::vector<int> UF;
+    
+    Union_Find(int sz)
+    {
+        UF.resize(sz + 1);
+        for (int i = 1; i <= sz; i++)
+            UF[i] = i;
+    }
+ 
+    int find(int x)
+    {
+         if (UF[x] == x) return x;
+         else return UF[x] = find(UF[x]);
+    }
+ 
+    void union(int x, int y)
+    {
+        x = find(x);
+        y = find(y);
+        UF[y] = x;
+    }
 };
